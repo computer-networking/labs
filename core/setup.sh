a7f4cb052ea4eb5ade41f7a7959fb6772d04a966
Facundo Victor <facundovt@gmail.com>
2020-02-23 17:59:12 -0300

    Add section to manage container storage

--- core/setup.sh
+++ core/setup.sh
@@ -38,6 +38,8 @@ function output::ok () {
 }
 
 ###############################################################################
+# Setup
+###############################################################################
 
 function setup::prepare_folders () {
     for path in "${LIBRARY_PATH}" "${IMAGES_PATH}" "${CONTAINERS_PATH}" ; do
@@ -47,11 +49,15 @@ function setup::prepare_folders () {
     done
 }
 
+###############################################################################
+# Image management
+###############################################################################
+
 function image::download () {
     declare archived_image="${1:-$DEFAULT_IMAGE}"
     declare image="${archived_image:0:-4}"
     declare image_path="${IMAGES_PATH}/${image}"
-    
+
     mkdir -p "$image_path"
     curl -L "${IMAGE_REPOSITORY_BASE}/${archived_image}" | tar -C "${image_path}/" -xf-
 
@@ -69,6 +75,51 @@ function image::remove () {
         output::error_and_die "Image to remove not stored in the expected path \"${image_path}\""
     fi
 }
+
+###############################################################################
+# Container management
+###############################################################################
+
+function container::generate_storage () {
+    declare archived_image="${1:-$DEFAULT_IMAGE}"
+    declare image="${archived_image:0:-4}"
+    declare image_path="${IMAGES_PATH:?}/${image}"
+    declare container_name="${2:-$(uuidgen)}"
+    declare container_path="${CONTAINERS_PATH:?}/${container_name}"
+
+    # 1- Ignore if the mount already exists
+    if grep -qs "${container_path}/merged" /proc/mounts ; then
+        return 0;
+    fi
+
+    # 2- Create overlayFS mount directories
+    mkdir -p $container_path/{diff,merged,work}
+
+    # 3- Mount the image
+    mount -t overlay overlay -o lowerdir="$image_path",upperdir="${container_path}/diff",workdir="${container_path}/work" "${container_path}/merged"
+}
+
+function container::remove_storage () {
+    declare archived_image="${1:-$DEFAULT_IMAGE}"
+    declare image="${archived_image:0:-4}"
+    declare image_path="${IMAGES_PATH:?}/${image}"
+    declare container_name="${2?Missing 2nd argument container name}"
+    declare container_path="${CONTAINERS_PATH:?}/${container_name}"
+
+    # 1- umount the storage
+    if grep -qs "${container_path}/merged" /proc/mounts ; then
+        umount "${container_path}/merged"
+    fi
+
+    # 2- Remove content
+    if [[ "$container_path" = $CONTAINERS_PATH* ]] ; then
+        rm -rf "${container_path:?}"
+    else
+        output::error_and_die "Container storage to remove not stored in the expected path \"${container_path}\""
+    fi
+}
+
+###############################################################################
 
 # Work in progress..
 setup::prepare_folders
